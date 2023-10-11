38ee4204cbcb8ed49765374e3e3e60e966f37455
macpherson <chukaihekire75@gmail.com>
2023-10-10 17:52:16 -0700

    first commit for task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints _putchar,
+ * followed by a new line
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
