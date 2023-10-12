e260f4abb61a1f4f62c1c023e45b48d4b70c3cf2
macpherson <chukaihekire75@gmail.com>
2023-10-12 14:24:40 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * _putchar - prints a character
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
