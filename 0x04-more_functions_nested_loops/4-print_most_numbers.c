27240d091ea7e9cad6bd5fe14ed25f7cfcc4c445
macpherson <chukaihekire75@gmail.com>
2023-10-12 14:57:55 -0700

    task 4 done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * print_most_numbers - prints number from 0 to 9
+ * _putchar - prints characters
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
