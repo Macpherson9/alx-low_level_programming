9928bbcf9fa657065459cb0549955de2771bc91b
macpherson <chukaihekire75@gmail.com>
2023-10-12 17:31:34 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,30 +11,30 @@
 int main(void)
 {
 	int i;
-	char str[];
 
 	for (i = 1; i < 101; i++)
 	{
 		if ((i % 15) == 0)
 		{
-			str[] = "FizzBuzz";
-			printf("s% ", str);
+			printf("FizBuzz ");
 		}
 		else if ((i % 5) == 0)
 		{
-			str[] = "Buzz";
-			printf("s% ", str);
+			printf("Buzz ");
 		}
 		else if ((i % 3) == 0)
 		{
-			str[] = "Fizz";
-			printf("s% ", str);
+			printf("Fizz ");
 		}
+		else if (i == 100)
+		{
+			printf("d%", i);
 		else
 		{
 			printf("d% ", i);
 		}
 	}
 	printf("\n");
+
 	return (0);
 }
