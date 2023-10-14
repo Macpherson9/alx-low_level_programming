0d500b1e236a50f5dc2e918bf23a7fc7023b23f4
macpherson <chukaihekire75@gmail.com>
2023-10-14 15:51:28 -0700

    with for loop

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table
+ * return: void
+ */
+
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if ((i * j) > 9)
+			{
+				_putchar((i * j)) / 10);
+				_putchar((i * j)) % 10);
+			}
+			else
+				_putchar((i * j));
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
