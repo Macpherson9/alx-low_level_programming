bf5f76cd9111d14fb35aa43dd242d383f8bcefab
macpherson <chukaihekire75@gmail.com>
2023-10-14 18:16:06 -0700

    okay

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,44 +4,44 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
+
 void print_times_table(int n)
 {
-	int i, j, k;
+	int i, j;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+				if (i * j < 10)
+			{_putchar((i * j) + '0');
+			if (j != n)
+			{_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			if (i * j + 1 < 10)
+				_putchar(' ');
+			}}
+			if (i * j > 9 && i * j < 100)
+			{_putchar((i * j) / 10 + '0');
+			_putchar((i * j) % 10 + '0');
+			if (j != n)
+			{_putchar(',');
+			_putchar(' ');
+			if (i * j + 1 < 100)
+				_putchar(' ');
 			}
-			_putchar('\n');
-		}
+			}
+			if (i * j > 99)
+			{_putchar((i * j) / 100 + '0');
+			_putchar(((i * j) % 100) / 10 + '0');
+			_putchar((i * j) % 10 + '0');
+			if (j != n)
+				{_putchar(',');
+				_putchar(' ');
+				}}}}
+		_putchar('\n');
 	}
 }
