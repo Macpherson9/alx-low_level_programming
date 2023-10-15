3928631766075c39b83feca8a031c9a1e893b2bb
macpherson <chukaihekire75@gmail.com>
2023-10-14 18:48:00 -0700

    only warning, will format later

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,41 +7,55 @@
 
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			k = i * j;
+			if (k < 10)
 			{
-				if (i * j < 10)
-			{_putchar((i * j) + '0');
+			_putchar(k + '0');
 			if (j != n)
-			{_putchar(',');
+			{
+			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
-			if (i * j + 1 < 10)
-				_putchar(' ');
-			}}
-			if (i * j > 9 && i * j < 100)
-			{_putchar((i * j) / 10 + '0');
-			_putchar((i * j) % 10 + '0');
-			if (j != n)
-			{_putchar(',');
-			_putchar(' ');
-			if (i * j + 1 < 100)
+			if (i * (j + 1) < 10)
 				_putchar(' ');
 			}
+			else
+				_putchar('\n');
+			}
+			if (k > 9 && k < 100)
+			{
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+			if (j != n)
+			{
+			_putchar(',');
+			_putchar(' ');
+			if (i * (j + 1) < 100)
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
 			}
 			if (i * j > 99)
-			{_putchar((i * j) / 100 + '0');
+			{
+			_putchar((i * j) / 100 + '0');
 			_putchar(((i * j) % 100) / 10 + '0');
 			_putchar((i * j) % 10 + '0');
 			if (j != n)
-				{_putchar(',');
-				_putchar(' ');
-				}}}}
-		_putchar('\n');
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			else
+				_putchar('\n');
+			}}}
 	}
 }
