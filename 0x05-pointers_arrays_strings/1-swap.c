e1e69cb90787767108d3da2ab644f23c418e3329
macpherson <chukaihekire75@gmail.com>
2023-10-16 15:14:31 -0700

    okay

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ * the pointer points to to 98
+ * @a: first interger variable
+ * @b: second integer variable
+ */
+
+void swap_int(int *a, int *b)
+{
+	int d = *b;
+
+	a = &b;
+	b = &d;
+}
