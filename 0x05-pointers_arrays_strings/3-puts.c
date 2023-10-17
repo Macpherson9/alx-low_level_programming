2683ff0e8c64db165cd66403222811ac1b4881cb
macpherson <chukaihekire75@gmail.com>
2023-10-17 13:19:48 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ * _puts - a function that prints a string
+ * @str: its a placeholder for the string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
