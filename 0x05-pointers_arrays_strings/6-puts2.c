7fc56694b08b11ee4ced68b1b45599496e389d97
macpherson <chukaihekire75@gmail.com>
2023-10-17 15:16:50 -0700

    okay

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * put2 - prints every other character
+ * @str: string
+ * Return: void
+ */
+
+void put2(char *str)
+{
+	int i, j;
+	int sum = 0;
+
+	for (j = 0; str[j] != '\n'; j++)
+		sum++;
+	for (i = 0; i <= sum; i = i + 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
