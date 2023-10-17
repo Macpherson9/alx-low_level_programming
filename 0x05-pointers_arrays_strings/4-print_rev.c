21a1bfc39b44582372064833b69a3b18f5f0f921
macpherson <chukaihekire75@gmail.com>
2023-10-17 13:45:28 -0700

    good logic

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the string in reverse
+ * @s: string placeholder
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int size = sizeof(s) / sizeof(s[0]);
+
+	if (size > 0)
+	{
+		for (i = size; i >= 0; i--)
+		{
+			_putchar(s[i - 1];
+		}
+		_putchar('\n');
+	}
+}
+
