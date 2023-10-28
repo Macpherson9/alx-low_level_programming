36f21fb96803d819a2b1734ea77bd8a4d6c1d5a5
macpherson <chukaihekire75@gmail.com>
2023-10-27 19:30:52 -0700

    trial 1

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+
+	if (argc <= 1)
+		printf("0\n");
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result = result + argv[i];
+	}
+	printf("%d\n", result);
+	return (0);
+}
