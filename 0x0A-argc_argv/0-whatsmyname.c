b0289db057ab03dedb1a3fcce60cf881629a6b9c
macpherson <chukaihekire75@gmail.com>
2023-10-27 17:42:30 -0700

    ok

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <std.io>
+#include "main.h"
+
+/**
+ * main - a program
+ * @argc: the count of the argument received
+ * @argv: the array of string
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
