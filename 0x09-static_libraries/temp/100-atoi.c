2111f6dfd45524572b5450bd4c6ac4b73a55a1d0
macpherson <chukaihekire75@gmail.com>
2023-10-29 17:31:09 -0700

    did the right files

--- 0x09-static_libraries/temp/100-atoi.c
+++ 0x09-static_libraries/temp/100-atoi.c
@@ -1,46 +1,31 @@
 #include "main.h"
-
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _atoi - convert a string to an integer
+ * @s: Pointer to the string
  *
- * Return: the int converted from the string
+ * Return: Always 0.
+ * Author: IanoNjuguna
  */
+
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	unsigned int sign = 1;
+	unsigned int result = 0;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
+	do {
+		if (*s == '-')
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
+			sign *= -1;
 		}
-		i++;
-	}
+		else if (*s >= '0' && *s <= '9')
+		{
+			result = (result * 10) + (*s - '0');
+		}
+		else if (result > 0)
+		{
+			break;
+		}
+	}	while (*s++);
 
-	if (f == 0)
-		return (0);
-
-	return (n);
+	return (result * sign);
 }
