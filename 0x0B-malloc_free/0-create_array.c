900c4c7f420736539a2de2dd4749f834af5d1693
macpherson <chukaihekire75@gmail.com>
2023-10-31 15:00:48 -0700

    spelled NULL correctly

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *result;
 
 	if (size == 0)
-		return (null);
+		return (NULL);
 	result = (char *)malloc(size * sizeof(char));
 	result[0] = c;
 	return (result);
