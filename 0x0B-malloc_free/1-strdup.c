d41df42ff48baf07d5ab122a268e4a861725672f
macpherson <chukaihekire75@gmail.com>
2023-11-04 15:20:33 -0700

    okay

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,27 +7,22 @@
  *
  * Return: pointer to the copied string (Success), NULL (Error)
  */
+
 char *_strdup(char *str)
 {
-	char *dup;
-	unsigned int i, len;
-
-	i = 0;
-	len = 0;
+	char *str1;
+	int n, len = 0;
 
 	if (str == NULL)
 		return (NULL);
-
 	while (str[len])
 		len++;
 
-	dup = malloc(sizeof(char) * (len + 1));
-
-	if (dup == NULL)
+	str1 = (char *) malloc((len + 1) * sizeof(char));
+	if (str1 == NULL)
 		return (NULL);
-
-	while ((dup[i] = str[i]) != '\0')
-		i++;
-
-	return (dup);
+	for (n = 0; n < len; n++)
+		str1[n] = str[n];
+	return (str1);
 }
+
