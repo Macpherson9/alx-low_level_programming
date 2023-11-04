00e3159f1422addfa1dea88d63fc5b50954d6ae4
macpherson <chukaihekire75@gmail.com>
2023-11-04 09:42:41 -0700

    okay

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	unsigned int i = 0;
+	int i;
 
 	if (size == 0)
 		return (NULL);
@@ -21,14 +21,8 @@ char *create_array(unsigned int size, char c)
 
 	if (p == NULL)
 		return (0);
-
-	while (i < size)
-	{
+	for (i = 0; i < size; i++)
 		*(p + i) = c;
-		i++;
-	}
-
 	*(p + i) = '\0';
-
 	return (p);
 }
