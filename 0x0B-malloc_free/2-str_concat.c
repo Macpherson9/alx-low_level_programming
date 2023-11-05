b9eea57c51afbc7db3d8f78dfe9d36646a30d7c7
macpherson <chukaihekire75@gmail.com>
2023-11-05 15:20:22 -0800

    okay

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,37 +8,42 @@
  *
  * Return: pointer to the new string created (Success), or NULL (Error)
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0, len2 = 0;
-	int j = 0, i;
-	char *str;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
 	while (s2 && s2[len2])
 		len2++;
 
-	str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-	if (str == NULL)
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
+
+	i = 0;
+	j = 0;
 
 	if (s1)
 	{
-		for (i = 0; i < len1; i++)
-			str[i] = s1[i];
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
+
 	if (s2)
 	{
 		while (i < (len1 + len2))
 		{
-			str[i] = s2[j];
+			s3[i] = s2[j];
 			i++;
 			j++;
 		}
 	}
-	str[i] = '\0';
+	s3[i] = '\0';
 
-	return (str);
+	return (s3);
 }
