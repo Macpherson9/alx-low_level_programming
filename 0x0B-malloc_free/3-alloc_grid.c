8897e24cc6b38b74499d2b09a85ed113e03c1411
macpherson <chukaihekire75@gmail.com>
2023-11-05 16:11:24 -0800

    okay3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,37 +9,28 @@
  * Return: pointer to the created matrix (Success)
  * or NULL (Error)
  */
+
 int **alloc_grid(int width, int height)
 {
-	int **arr;
+	int **ptr;
 	int i, j;
 
-	if (height <= 0 || width <= 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-
-	arr = (int **) malloc(sizeof(int *) * height);
-
-	if (arr == NULL)
+	ptr = malloc(height * sizeof(int *));
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *) malloc(sizeof(int) * width);
-		if (arr[i] == NULL)
-		{
-			free(arr);
-			for (j = 0; j <= i; j++)
-				free(arr[j]);
+		ptr[i] = malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
 			return (NULL);
-		}
 	}
-
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-		{
-			arr[i][j] = 0;
-		}
+			ptr[i][j] = 0;
 	}
-	return (arr);
+	return (ptr);
 }
